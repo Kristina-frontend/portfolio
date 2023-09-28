14a9475ea343a6c4d33cce87e8a5ac748d5f5679
Kristina-frontend <120197972+Kristina-frontend@users.noreply.github.com>
2023-09-28 11:36:36 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -121,5 +121,23 @@ $(document).ready(function () {
     $(".nav").toggleClass("active")
   })
 
+  // up arrow
+  $(window).scroll(function(){
+    if($(this).scrollTop()>200){
+      $(".btn-up").fadeIn()
+    }
+    else{
+      $(".btn-up").fadeOut()
+    }
+  })
+
+  $(".btn-up").click(function (e) { 
+    e.preventDefault();
+    $("html, body").animate({
+      scrollTop: 0
+    }, 800)
+
+  });
+
 
 });
